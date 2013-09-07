a966dc463f141beb28ec279b32a9f68f343403b8
lmartinez <lmartinez@tradestation.com>
2013-09-06 19:01:32 -0600

    ApiController & DataController

--- Customary/Areas/App/Controllers/MetadataController.cs
+++ Customary/Areas/App/Controllers/MetadataController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace Custom.Areas.App.Controllersustom.Controllers
+namespace Custom.Areas.App.Metadata.Controllers
 {
     using Custom.Models;
     using Custom.Controllers;
---
+++ Customary/Controllers/DataController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Custom.Controllers
+{
+    public class DataController : Controller
+    {
+        //
+        // GET: /Data/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /Data/{typeID}/Details/{entityID}
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /Data/{typeID}/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Data/{typeID}/Create
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Data/{typeID}/Edit/{entityID}
+
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Data/{typeID}/Edit/{entityID}
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Data/{typeID}/Delete/{entityID}
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Data/{typeID}/Delete/{entityID}
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
